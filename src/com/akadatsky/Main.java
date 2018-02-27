04cf9392c067ab07e9f654949549de18272a9aad
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2018-02-27 18:45:22 +0200

    initial commit

---
+++ src/com/akadatsky/Main.java
@@ -0,0 +1,13 @@
+package com.akadatsky;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test Primes");
+    }
+
+    private static boolean isPrime(int number) {
+        return false;
+    }
+
+}
