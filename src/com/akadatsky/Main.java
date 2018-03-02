574760d6f32eee4df83a48356fa4fad18c943b0c
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2018-03-02 17:11:26 +0200

    fix unworking code

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test Primes");
+        System.out.println("Test number 13: " + isPrime(13));
+        System.out.println("Test number 15: " + isPrime(15));
     }
 
     private static boolean isPrime(int number) {
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
                 return false;
-                break;
             }
         }
         return true;
